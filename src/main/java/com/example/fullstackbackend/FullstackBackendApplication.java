8584ab54b082719874467c16f4ced1741cc45575
Shivani Hagone <shivanihagone@gmail.com>
2024-04-19 10:58:28 +0530

    First commit

---
+++ src/main/java/com/example/fullstackbackend/FullstackBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.fullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackendApplication.class, args);
+		System.out.println("loaded...");
+	}
+
+}
